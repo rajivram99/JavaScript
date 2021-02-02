711926a16414c7f7e4dddf8dd8d02b30ad06e097
rajiv77777 <44100247+rajiv77777@users.noreply.github.com>
2021-02-02 09:58:46 +0530

    Console log and error

---
+++ script.js
@@ -0,0 +1,11 @@
+(function() {
+"use strict";
+
+
+var variable1 = 2 , variable2 = "helllo";
+
+console.log("varibale 1 and variable 2 -> ",variable1 , variable2);
+console.error("variable 2" , variable2);
+
+
+})();
